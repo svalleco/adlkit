198af66773e5a232e8d024c90444f6c09c7f7bb8
Grayson Hilliard <wghilliard@anomalousdl.com>
2017-09-01 04:58:06 -0500

    BUG: updated setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import setuptools
 
 setuptools.setup(
-    name='adlkit',
-    version='0.4.0',
-    author='Amir Farbin, William Hilliard, Ryan Reece',
-    author_email='wghilliard@anomalousdl.com',
-    url='https://github.com/anomalousdl/adlkit',
-    install_requires=[
-        'keras',
-        'numpy',
-        'h5py',
-        'tensorflow'
-    ],
-    packages=setuptools.find_packages()
+        name="adlkit",
+        version="0.4.0",
+        author="Amir Farbin, William Hilliard, Ryan Reece",
+        author_email="wghilliard@anomalousdl.com",
+        url="https://github.com/anomalousdl/adlkit",
+        install_requires=[
+            "keras",
+            "numpy",
+            "h5py",
+            "theano",
+            "billiard"
+        ],
+        packages=setuptools.find_packages()
 )
