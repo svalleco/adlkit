6cd934461b5b0aab4e60f133aac21a462bfb610b
Grayson Hilliard <wghilliard@anomalousdl.com>
2017-09-25 15:36:55 -0500

    ENH: better versioning

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import setuptools
 
+from adlkit import __version__
+
 setuptools.setup(
         name="adlkit",
-        version="0.4.0",
+        version=__version__,
         author="Amir Farbin, William Hilliard, Ryan Reece",
         author_email="wghilliard@anomalousdl.com",
         url="https://github.com/anomalousdl/adlkit",
